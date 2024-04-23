1c8a9fd73d064191f683b7dfaf0f332b3d47d61f
nguyenbn7 <nguyenbn.email@gmail.com>
2024-04-23 18:09:15 +0700

    add particles

--- web-app/src/routes/(landing)/+page.svelte
+++ web-app/src/routes/(landing)/+page.svelte
@@ -6,6 +6,9 @@
 	import Section from '$lib/component/section.svelte';
 	import ShowCaseInfo from '$lib/home/show-case-info.svelte';
 	import ShowCaseWrap from '$lib/home/show-case-wrap.svelte';
+	import Particles, { particlesInit } from '@tsparticles/svelte';
+	import { onMount } from 'svelte';
+	import { loadFull } from 'tsparticles';
 
 	const showCaseFilters = [
 		{
@@ -73,7 +76,137 @@
 			type: 'web'
 		}
 	];
+
+	/**
+	 * @type {typeof Particles}
+	 */
+	let ParticlesComponent;
+
+	/**
+	 * @type {import("@tsparticles/engine").RecursivePartial<import("@tsparticles/engine").IOptions>}
+	 */
+	let particlesConfig = {
+		particles: {
+			number: {
+				value: 175
+			},
+			color: {
+				value: ['#fbb03b', '#ffffff']
+			},
+			shape: {
+				type: 'circle'
+			},
+			opacity: {
+				value: {
+					min: 0.5,
+					max: 0.75
+				},
+				animation: {
+					enable: true,
+					speed: 1,
+					sync: false,
+					mode: 'random',
+					startValue: 'random'
+				}
+			},
+			size: {
+				value: {
+					min: 0.01,
+					max: 5
+				},
+				animation: {
+					enable: true,
+					speed: 2.5,
+					sync: false,
+					mode: 'random',
+					startValue: 'random'
+				}
+			},
+			move: {
+				enable: true,
+				speed: 0.15,
+				direction: 'none',
+				random: true,
+				straight: false,
+				outModes: 'out',
+				attract: {
+					enable: false,
+					rotate: {
+						x: 600,
+						y: 1200
+					}
+				}
+			}
+		},
+		interactivity: {
+			detect_on: 'canvas',
+			events: {
+				onHover: {
+					enable: true,
+					mode: 'bubble'
+				},
+				onClick: {
+					enable: true,
+					mode: 'push'
+				},
+				resize: {
+					enable: true
+				}
+			},
+			modes: {
+				grab: {
+					distance: 400,
+					line_linked: {
+						opacity: 1
+					}
+				},
+				bubble: {
+					distance: 83.91608391608392,
+					size: 1,
+					duration: 3,
+					opacity: 1,
+					speed: 3
+				},
+				repulse: {
+					distance: 200,
+					duration: 0.4
+				},
+				push: {
+					particles_nb: 2
+				},
+				remove: {
+					particles_nb: 2
+				}
+			}
+		}
+	};
+
+	/**
+	 * @param {CustomEvent<any>} $event
+	 */
+	function onParticlesLoaded($event) {
+		const particlesContainer = $event.detail.particles;
+	}
+
+	/** {@link https://github.com/tsparticles/svelte/?tab=readme-ov-file#ssr} */
+	void particlesInit(async (engine) => {
+		await loadFull(engine);
+	});
+
+	onMount(async () => {
+		const module = await import('@tsparticles/svelte');
+
+		ParticlesComponent = module.default;
+	});
 </script>
+
+<svelte:component
+	this={ParticlesComponent}
+	id="background-landing"
+	class="absolute size-full -z-10"
+	options={particlesConfig}
+	on:particlesLoaded={onParticlesLoaded}
+/>
 
 <h1 class="text-[64px] font-bold w-full text-center">Web tools</h1>
 
@@ -146,4 +279,8 @@
 	.filter-active {
 		color: #1bab91;
 	}
+
+	/* :global(html) {
+		@apply bg-[#212121];
+	} */
 </style>
